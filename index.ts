a1b0fe2b83f7139ce89a8fe03503bddbf1f1cf43
Shourov <shourov.ahmed@student.ap.be>
2023-05-16 13:32:15 +0200

    fix undefined reading error with cardfaces + set correct limit of 175 per page

--- index.ts
+++ index.ts
@@ -11,59 +11,109 @@ app.use(express.static("public"));
 app.use(express.json({limit: '1mb'}));
 app.use(express.urlencoded({extended: true}));
 
+let pics = [{name: '', img: '', rarity: ''}];
+
 app.get("/home", (req, res) => {
     res.render("homepage");
 });
 
+interface Card {
+    name: string,
+    img: string,
+    rarity: string
+}
 
-let pics = [{name: '', img: '', rarity: ''}];
+
+
 app.post("/home", async (req, res) => {
 
-    try
-    {let text = req.body.search; console.log(text);
-    let response = await fetch(`https://api.scryfall.com/cards/search?q=${text}`);
-    let cards = await response.json();
+    try{
+        let text = req.body.search; 
+        console.log(text);
+        let response = await fetch(`https://api.scryfall.com/cards/search?q=lore=${text}`); // Lore zoekt overal op kaart
+        let cards = await response.json();
+        //console.log(cards);
 
-    if(cards.object != "error"){
-        pics = [{name: '', img: '', rarity: ''}];
-        let total_cards = cards.total_cards;
-        if(total_cards > 40)
-        {        
-            total_cards = 40;
-        }
-        for (let i = 0; i < total_cards; i++){
-            if(cards.data[i].card_faces){
-                for(let j = 0; j < cards.data[i].card_faces.length; j++){
-                    pics.push({
-                        name: cards.data[i].name,
-                        img: cards.data[i].card_faces[j].image_uris.border_crop,
-                        rarity: cards.data[i].card_faces[j].rarity
-                    });
+        if(cards.object != "error"){
+            pics = [{name: '', img: '', rarity: ''}];
+            let total_cards = cards.total_cards;
+            if(total_cards > 175)
+            {        
+                total_cards = 175;
+            }
+            for (let i = 0; i < total_cards; i++){
+                if(cards.data[i].card_faces){
+                    for(let j = 0; j < cards.data[i].card_faces.length; j++){
+                        if(cards.data[i].card_faces[j].image_uris){ // Sommige kaarten hebben enkel 1 image, in de main card object. sommige verschillende imgs in de card_faces objecten
+                            pics[i] = {
+                                name: cards.data[i].name,
+                                img: cards.data[i].card_faces[j].image_uris.normal,
+                                rarity: cards.data[i].card_faces[j].rarity
+                            };
+                        }
+
+                        else{
+                            pics[i] = {
+                                name: cards.data[i].name,
+                                img: cards.data[i].image_uris.normal,
+                                rarity: cards.data[i].rarity
+                            };
+                        }
+                    }
+                    
                 }
-                
+
+                else{
+                    pics[i] = {
+                        name: cards.data[i].name,
+                        img: cards.data[i].image_uris.normal,
+                        rarity: cards.data[i].rarity
+                    };
+                }
+                    
             }
-            else{
-                pics.push({
-                    name: cards.data[i].name,
-                    img: cards.data[i].image_uris.border_crop,
-                    rarity: cards.data[i].rarity
-                });
+            
+                        for (let i = 0; i < 175; i++){
+                if(cards.data[i].card_faces){
+                    for(let j = 0; j < cards.data[i].card_faces.length; j++){
+                        if(cards.data[i].card_faces[j].image_uris){ // Sommige kaarten hebben enkel 1 image, in de main card object. sommige verschillende imgs in de card_faces objecten
+                            pics[i] = {
+                                name: cards.data[i].name,
+                                img: cards.data[i].card_faces[j].image_uris.normal,
+                                rarity: cards.data[i].card_faces[j].rarity
+                            };
+                        }
+                        
+                        else{
+                            pics[i] = {
+                                name: cards.data[i].name,
+                                img: cards.data[i].image_uris.normal,
+                                rarity: cards.data[i].rarity
+                            };
+                        }
+                    }
+                    
+                }
+                else{
+                    pics[i] = {
+                        name: cards.data[i].name,
+                        img: cards.data[i].image_uris.normal,
+                        rarity: cards.data[i].rarity
+                    };
+                }
+
             }
-                
+            console.table(pics, ["name", "rarity"]);
+            return res.render("homepage", {pics});
         }
-        console.table(pics);
-        //console.log(x.data[0].name);
-        return res.render("homepage", {pics});
+
+        else{
+            return res.render("homepage", {error: "error"});
+        }
     }
 
-    else{
-        return res.render("homepage", {error: "error"});
-    }}
     catch(e:any){
         console.log(e);
-    }
-    finally{
-        res.render("homepage");
     }
     
 });
