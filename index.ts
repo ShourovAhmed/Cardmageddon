dd9730febcede75f0588dc2fc6b9ae5ce0064966
Staelens Sascha <sascha.staelens@gmail.com>
2023-05-24 20:33:50 +0200

    api call vanuit db werkt deels

--- index.ts
+++ index.ts
@@ -42,9 +42,60 @@ app.set("view engine", "ejs");
 
 app.use('*/public',express.static('public/'));
 
+//making funct to get image from certain id (from variation in db)
+
+let getPic= async (varId:string, ) => {
+
+
+    let id= varId;
+    let response = await fetch(`https://api.scryfall.com/cards/${id}`); 
+    let card = await response.json();
+    let cardImg=card.image_uris.normal
+    //console.log(`getPic:${cardImg}`);
+    return cardImg;     //returns the img src needed. 
+    
+    
+}
+
+
+let makeIdList=(cards:CardS[],cardsIds:string[]) => {
+
+    
+
+    for(let i=0;i<cards.length;i++){
+
+        let card=cards[i].variations[0];
+        
+        
+        
+
+        cardsIds.push(card.id);
 
 
 
+    }
+    //console.log(`copyArray: ${cardToIds}`);
+    return cardsIds;
+
+
+
+}
+
+let makeImgList =async(cardsIds:string[],cardImgs:string[]) => {
+
+    for(let i=0;i<cardsIds.length;i++){
+
+        let cardImage=await getPic(cardsIds[i]);
+
+        cardImgs.push(cardImage);       
+
+
+    }
+    //console.log(`listimages: ${cardImgs}`);
+    return cardImgs;
+
+
+}
 
 
 
@@ -55,20 +106,74 @@ try{
 
     let deckCollection= client.db("userData").collection("decks");
 
-    let decks= await deckCollection.find<Deck>({}).toArray();
+    let decks =await deckCollection.find<Deck>({}).toArray();//REMOVE only needed TEMP untill removind decks
 
-    let id="51833cff-6519-4806-8d91-0040e9a02189";
 
-    let response = await fetch(`https://api.scryfall.com/cards/${id}`);  //finds car on Id  cant find on id from db but can find it if using variant id
 
-    let card = await response;
+    let decksDatabase= await deckCollection.find<Deck>({}).toArray();
+    
+    let chosenDeck=decksDatabase[0];//later deckkeuze aanmaken
+    let cards:CardS[]= chosenDeck.cards!    //non-null assertion operator ? might work now
 
-    console.log(card);
+    
+    let cardsIds:string[]=[]; 
+    cardsIds=makeIdList(cards,cardsIds);    //this array only contains variableIds used for api//also allows deleting in later uses without touching original Database
+
+
+    let cardImgs:string[]=[]; 
+    cardImgs=await makeImgList(cardsIds,cardImgs); 
+
+    //console.log("All Ids deck 1:"+ cardsIds);
+    
+
+
+
+
+
+
+///----Notes for next:   We have Ids wich can be deleted from and we have api calll function, 
+//                        so now i combine in loop etc later add deck selector?
+    
+
+
+
+
+
+
+
+    // let id="5e2465d3-405d-487d-b6e9-d2ec8b920201";      //for early testing only
+
+    // let response = await fetch(`https://api.scryfall.com/cards/${id}`);  //finds card on Id  cant find on id from db but can find it if using variant id
+
+    // let card = await response.json();
+
+    // console.log(card.image_uris.normal);
+
+    let cardImg= await getPic("5e2465d3-405d-487d-b6e9-d2ec8b920201");
+
+
+
+
+    //TESTING ARRAY THINGY IGNORE
+/*
+    let arr1: string[] = ["one", "two", "three"];
+    let arr2=arr1;
+    let index= arr2.indexOf("two");
+    if(index!=-1){
+        arr2.splice(index,1);
+    }
+
+    console.log(arr1);
+    console.log(arr2);
+
+    */
     
     
 
     res.render("drawtest",{
-        decks: decks
+        decks: decks,
+        cardImg,    //remove later
+        cardImgs:cardImgs
     });
 
 }catch(e){
